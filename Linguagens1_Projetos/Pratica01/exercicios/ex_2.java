d83ed99bce769baee50faeed21275ed8a2aa5042
Andre Lebrao <andledrao@gmail.com>
2020-03-05 14:25:15 -0300

    quase pronto

---
+++ Linguagens1_Projetos/Pratica01/exercicios/ex_2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * ex_2
+ */
+public class ex_2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome1 = scanner.nextLine();
+        System.out.println("Digite sua idade: ");
+        int idade1 = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Digite seu nome: ");
+        String nome2 = scanner.nextLine();
+        System.out.println("Digite sua idade: ");
+        int idade2 = scanner.nextInt();
+
+        int dif = Math.abs(idade1-idade2);
+        System.out.println("dif: "+dif);
+    }
+}
