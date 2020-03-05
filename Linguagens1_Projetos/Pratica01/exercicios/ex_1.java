0ad7188ab400915ddc819a7d3bfe4aaddb100770
Andre Lebrao <andledrao@gmail.com>
2020-03-05 11:15:52 -0300

    comentario scanner.nextInt()

--- Linguagens1_Projetos/Pratica01/exercicios/ex_1.java
+++ Linguagens1_Projetos/Pratica01/exercicios/ex_1.java
@@ -14,7 +14,8 @@ public class ex_1 {
         String nome = scanner.nextLine();
         System.out.println("Informe seu telefone : ");
         int tel = scanner.nextInt();
-        scanner.nextLine();
+        // toda vez que usar scanner.nextInt() precisa dar um scanner.nextLine() pq o nextint nao escaneia o enter que da pra pular linha 
+        scanner.nextLine(); 
         System.out.println("Informe seu email : ");
         String email = scanner.nextLine();
         
