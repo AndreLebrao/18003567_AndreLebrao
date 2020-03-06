c5064a442b0edc00528131d1683bf4c3821d165d
Andre Lebrao <andledrao@gmail.com>
2020-03-06 18:56:46 -0300

    programa na metade

---
+++ Linguagens1_Projetos/Pratica01/exercicios/ex_4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * ex_4
+ */
+public class ex_4 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite uma frase para ver se é ou não palíndromo: ");
+        Srting frase = scanner.nextLine();
+        
+    }
+}
