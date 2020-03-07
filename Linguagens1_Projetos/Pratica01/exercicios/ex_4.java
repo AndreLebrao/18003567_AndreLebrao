10e282ab358eac97ff8129a7d8a057f0f8efc072
Andre Lebrao <andledrao@gmail.com>
2020-03-07 18:25:11 -0300

    funciona mas deve ter um jeito melhor de fazer

--- Linguagens1_Projetos/Pratica01/exercicios/ex_4.java
+++ Linguagens1_Projetos/Pratica01/exercicios/ex_4.java
@@ -6,10 +6,24 @@ import java.util.Scanner;
 public class ex_4 {
 
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
-
+        StringBuffer frase = new StringBuffer();
+        StringBuffer inverso = new StringBuffer();
+        
         System.out.println("Digite uma frase para ver se é ou não palíndromo: ");
-        Srting frase = scanner.nextLine();
+        String digitado = scanner.nextLine();
+        frase.append(digitado);
+        inverso.append(frase.toString());
+
+        inverso.reverse();
+        if (frase.equals(inverso)) {
+            System.out.println("É um palíndromo!");
+        } else {
+            System.out.println("Não é um palíndromo!");
+        }
+
+        
         
     }
 }
