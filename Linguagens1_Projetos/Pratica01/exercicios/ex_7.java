3f98b07e8b4d70d616dbd95387ea90658265ce74
Andre Lebrao <andledrao@gmail.com>
2020-03-08 17:52:55 -0300

    mano?

--- Linguagens1_Projetos/Pratica01/exercicios/ex_7.java
+++ Linguagens1_Projetos/Pratica01/exercicios/ex_7.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
+
 /**
  * ex_7
  */
 public class ex_7 {
 
     public static void main(String[] args) {
+     Scanner scanner = new Scanner(System.in);
+     
+     Integer auxInteger = 0;
+     System.out.println("Digite um número inteiro: ");
+     String valor = scanner.nextLine();
+    char [] valor_char = valor.toCharArray();
+
+    for(char c: valor_char){
         
+
+    }
+    // for (int i = 0; i < valor.length(); i++) {
+    //     auxInteger += auxInteger.parseInt(valor.;   
+    // }
+    System.out.println(valor.toCharArray());
+    
     }
 }
