3a6c11a41fbd81bac311706da8cfe755ef6bea8f
Andre Lebrao <andledrao@gmail.com>
2020-03-09 12:38:51 -0300

    organizando

--- Linguagens1_Projetos/Pratica02/aula/Exemplo01/ContaTestDrive.java
+++ Linguagens1_Projetos/Pratica02/aula/Exemplo01/ContaTestDrive.java
@@ -11,16 +11,19 @@ public class ContaTestDrive {
         //Instancia conta - coloca a referencia de um objeto
         c1 = new Conta();
         c1.cliente = new Cliente();
-        c1.cliente.nome = "Odair";
         Conta c2 = new Conta();
         c2.cliente = new Cliente();
+
+        c1.cliente.nome = "Odair";
         c2.cliente.nome = "Goku";
         System.out.println("Titular: "+c1.cliente.nome);
         System.out.println("Titular c2: "+c2.cliente.nome);
         c1.saldo = 2000;
         c1.visualizarSaldo();
+        
         //Todo atributo que não tem um valor atribuido tem um default, por nesse caso (double) comeca como 0.0
         // c2.visualizarSaldo();
+        
         c1.sacar(300);
         c1.sacar(3000);
         c1.visualizarSaldo();
