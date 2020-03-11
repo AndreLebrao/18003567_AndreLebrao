df61aac0564c6c812bee7df25dfc81168cd7af7a
Andre Lebrao <andledrao@gmail.com>
2020-03-11 12:24:57 -0300

    1 e 2

---
+++ Linguagens1_Projetos/Pratica02/exercicios/Exercicio1/Jogada.java
@@ -0,0 +1,12 @@
+package Exercicio1;
+
+/**
+ * Resultados
+ */
+public class Jogada {
+
+    public boolean Pedra = false;
+    public boolean Papel = false;
+    public boolean Tesoura = false;
+    
+}
---
+++ Linguagens1_Projetos/Pratica02/exercicios/Jogador.java
@@ -0,0 +1,12 @@
+package Exercicio1;
+import Exercicio1.Jogada;
+
+/**
+ * Jogador
+ */
+public class Jogador {
+
+    public String nome;
+    public Jogada jogada;
+    
+}
