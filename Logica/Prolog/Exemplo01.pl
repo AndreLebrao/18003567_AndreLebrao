5f1eb1425297bafc7b7f749a3dd38a96067793f0
Andre Lebrao <andledrao@gmail.com>
2020-03-13 10:17:16 -0300

    declarando fatos

---
+++ Logica/Prolog/Exemplo01.pl
@@ -0,0 +1,14 @@
+% FATOS
+% Predicado: come(X,Y)
+% Significado: X come Y
+come(urso,peixe).
+come(urso,raposa).
+come(cervo,grama).
+% Predicado: animal(X)
+% Significado: X é um animal
+animal(urso).
+animal(peixe).
+animal(raposa).
+animal(cervo).
+planta(grama).
+%com os fatos declarados podemos fazer perguntas
