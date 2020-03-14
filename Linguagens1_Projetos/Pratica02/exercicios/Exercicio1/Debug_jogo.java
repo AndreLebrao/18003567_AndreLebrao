4a572e28cc4ec53208b77818d54d4eccbf180c81
andledrao <andledrao@gmail.com>
2020-03-14 15:56:36 -0300

    FUNCIONA

--- Linguagens1_Projetos/Pratica02/exercicios/Exercicio1/Debug_jogo.java
+++ Linguagens1_Projetos/Pratica02/exercicios/Exercicio1/Debug_jogo.java
@@ -10,9 +10,10 @@ public class Debug_jogo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Jogo jogo = new Jogo();
-        jogo.player1.cadastrarJogador();
-        jogo.player2.cadastrarJogador();
-        jogo.player1.jogada.cadastrarJogada();
+        jogo.player1.nome = "andre";
+        jogo.player2.nome = "jatobas";
+        jogo.player1.jogada.codigoJogada = 1;
+        jogo.player2.jogada.codigoJogada = 1;
         jogo.comecarJokenpo();
         
         scanner.close();
