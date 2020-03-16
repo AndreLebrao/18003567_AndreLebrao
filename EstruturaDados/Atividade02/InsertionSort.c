bb060994c3189e581bf802fe51ab2c9cfe49a6db
andledrao <andledrao@gmail.com>
2020-03-16 14:51:20 -0300

    nao funciona

--- EstruturaDados/Atividade02/InsertionSort.c
+++ EstruturaDados/Atividade02/InsertionSort.c
@@ -1,29 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 100
 
-// void Isort(int entrada[], int saida[]);
+void insertionSort(int entrada[], int saida[]);
+void printArray(int array[]);
 
 int main(int argc, char const *argv[]){
     
     int entrada[] = {7,8,5,7,10,3,2,1,0,6};
     int len = (sizeof(entrada)/sizeof(entrada[0]));
+    int saida[len];
+    
     printf("size: %d\n", (int) len);
 
-    
+    for (int i = 0; i < len; i++){
+        printf("%d  ",(int) entrada[i]);
+    }
+
+    insertionSort(entrada,saida);
+    printf("\n");
+    for (int i = 0; i < len; i++){
+        printf("%d  ",(int) entrada[i]);
+    }
+    printf("\n");
     return 0;
 }
 
 
-// void Isort(int entrada[], int saida[]){
-
-//     for (int j = 2; j < MAX ; j++)
-//     {
-//         do
-//         {
-//             int chave = entrada[j]; 
-//         } while (i>0 && A[i]>chave);
+void insertionSort(int entrada[], int saida[]){
+    int len = (sizeof(entrada)/sizeof(entrada[0]));
+    for (int j = 1; j < len  ; j++){
+        int chave = entrada[j];
+        //inserir A[j] na sequencia ordenada A[1..j-1]
+        int i = j-1;
+        while(i>0 && entrada[i]>chave){
+            entrada[i+1] = entrada[i];
+            i--;
+        }
+        entrada[i+1] = chave;
         
-//     }
+    }
     
-// }
+}
