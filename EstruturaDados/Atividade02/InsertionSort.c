9f7019904038a2cbdd19628366a64e5327f10d02
andledrao <andledrao@gmail.com>
2020-03-16 15:36:10 -0300

    completo

--- EstruturaDados/Atividade02/InsertionSort.c
+++ EstruturaDados/Atividade02/InsertionSort.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void insertionSort(int entrada[], int saida[]);
+void insertionSort(int entrada[], int len);
 void printArray(int array[]);
 
 int main(int argc, char const *argv[]){
     
-    int entrada[] = {7,8,5,7,10,3,2,1,0,6};
+    int entrada[] = {9,8,5,7,10,3,2,1,0,6};
     int len = (sizeof(entrada)/sizeof(entrada[0]));
-    int saida[len];
     
     printf("size: %d\n", (int) len);
 
@@ -16,7 +15,7 @@ int main(int argc, char const *argv[]){
         printf("%d  ",(int) entrada[i]);
     }
 
-    insertionSort(entrada,saida);
+    insertionSort(entrada, len);
     printf("\n");
     for (int i = 0; i < len; i++){
         printf("%d  ",(int) entrada[i]);
@@ -26,13 +25,14 @@ int main(int argc, char const *argv[]){
 }
 
 
-void insertionSort(int entrada[], int saida[]){
-    int len = (sizeof(entrada)/sizeof(entrada[0]));
+void insertionSort(int entrada[], int len){
+    // int len = sizeof(entrada)/sizeof(entrada[0]);
+
     for (int j = 1; j < len  ; j++){
         int chave = entrada[j];
-        //inserir A[j] na sequencia ordenada A[1..j-1]
+        // entrada[j-1] = chave;
         int i = j-1;
-        while(i>0 && entrada[i]>chave){
+        while(i>=0 && entrada[i]>chave){
             entrada[i+1] = entrada[i];
             i--;
         }
