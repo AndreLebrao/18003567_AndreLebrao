2f7b76c7bbe74f819429e6478b7aa083a9f12b25
JayJatoba <jguilhermejatoba@hotmail.com>
2020-06-08 11:54:23 -0300

    Mobile member name
    Mobile Member name

--- Linguagens1_Projetos/Atividade02/src/br/maua/models/MobileMember.java
+++ Linguagens1_Projetos/Atividade02/src/br/maua/models/MobileMember.java
@@ -4,14 +4,14 @@ import br.maua.enums.MemberType;
 import br.maua.enums.SystemTime;
 import br.maua.interfaces.MemberPostarMensagem;
 
-public class MobileMembers extends Member implements MemberPostarMensagem{
+public class MobileMember extends Member implements MemberPostarMensagem {
 
-    public MobileMembers(String nomeUsuario, String email) {
+    public MobileMember(String nomeUsuario, String email) {
         super(nomeUsuario, email);
         this.cargo = MemberType.MOBILE_MEMBER;
         this.assRegular = "Happy Coding!";
-        this.assExtra = "Happy_C0d1ng.Maskers";
-    }
+        this.assExtra = "Happy_C0d1ng.Maskers";}
+
     @Override
     public void postarMensagem (String mensagem) {
         if (this.sTime.equals(SystemTime.REGULAR)) {
