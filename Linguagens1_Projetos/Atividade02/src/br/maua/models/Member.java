b60c936ff936d5d92df8bd904c6bc3b94eb1aa5b
André Lebrão <61150819+andledrao@users.noreply.github.com>
2020-06-08 10:44:09 -0300

    MobileMember, correcao Member e interface

--- Linguagens1_Projetos/Atividade02/src/br/maua/models/Member.java
+++ Linguagens1_Projetos/Atividade02/src/br/maua/models/Member.java
@@ -1,7 +1,33 @@
 package br.maua.models;
 
+import br.maua.enums.MemberType;
+import br.maua.enums.SystemTime;
+
 public abstract class Member {
     
     private String nomeUsuario;
     private String email;
+    protected String assRegular;
+    protected String assExtra;
+    protected MemberType cargo;
+    protected SystemTime sTime;
+
+    public Member(String nomeUsuario, String email) {
+        this.nomeUsuario = nomeUsuario;
+        this.email = email;
+        sTime = SystemTime.REGULAR;
+    }
+
+    public void trocarSystemTime(){
+        if (sTime.equals(SystemTime.REGULAR)) {
+            sTime = SystemTime.EXTRA;
+        } else {
+            sTime = SystemTime.REGULAR;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Member [email=" + email + ", nomeUsuario=" + nomeUsuario + "]";
+    }
 }
