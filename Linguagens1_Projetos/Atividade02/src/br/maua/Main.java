2063e32cd43c6d0f4938e4b6a45e8cced56b9358
André Lebrão <61150819+andledrao@users.noreply.github.com>
2020-06-08 11:14:07 -0300

    Criacao Menu e run()

--- Linguagens1_Projetos/Atividade02/src/br/maua/Main.java
+++ Linguagens1_Projetos/Atividade02/src/br/maua/Main.java
@@ -1,10 +1,12 @@
 package br.maua;
 
+import br.maua.sistema.Menu;
+
 //André Lebrão A Ribeiro 18.00356-7
 //João Guilherme Martins Jatobá 18.01790-8
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Menu.run();
     }
 }
