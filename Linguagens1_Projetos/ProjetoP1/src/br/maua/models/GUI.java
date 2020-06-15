c50f2f6b9e6f810f81dbee44effbee755905afce
André Lebrão <61150819+andledrao@users.noreply.github.com>
2020-06-15 14:14:10 -0300

    implementando Nova Venda

--- Linguagens1_Projetos/ProjetoP1/src/br/maua/models/GUI.java
+++ Linguagens1_Projetos/ProjetoP1/src/br/maua/models/GUI.java
@@ -1,20 +1,53 @@
 package br.maua.models;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
+import br.maua.enums.MetodoPagamento;
 import br.maua.interfaces.LerNumero;
 
 public class GUI implements LerNumero{
+    private static ArrayList<Pedido> listaPedidos = new ArrayList<>();
+    
     public static void run() {
         Scanner scanner = new Scanner(System.in);
         int resposta;
-        System.out.println("Pizzaria o Rato que Ri:\n1 - Nova venda\n2 - Verificar pedidos\n3 - Alterar pedidos\n0 - Sair\n\n");
+        System.out.println("\nPizzaria o Rato que Ri:\n1 - Nova venda\n2 - Verificar pedidos\n3 - Alterar pedidos\n0 - Sair\n");
         resposta = LerNumero.lerInt(scanner.nextLine());
         while (resposta!=0) {
             switch (resposta) {
                 case 1:
                     System.out.println("Descricao do pedido: ");
-                    
+                    String descricao = scanner.nextLine();
+                    System.out.println("Preco do pedido: ");
+                    double preco = LerNumero.lerDouble(scanner.nextLine());
+                    if(preco <= 0){
+                        System.out.println("Preco invalido...");
+                        break;
+                    }
+                    System.out.println("Método de pagamento:\n1 - Dinheiro\n2 - Debito\n3 - Credito\n4 - Vale Alimentacao\n5 - VR\n");
+                    MetodoPagamento mPagamento = MetodoPagamento.DINHEIRO;
+                    int intMetodoPagamento = LerNumero.lerInt(scanner.nextLine());
+                    switch (intMetodoPagamento) {
+                        case 1:
+                            mPagamento = MetodoPagamento.DINHEIRO;
+                            break;
+                        case 2:
+                            mPagamento = MetodoPagamento.DEBITO;
+                            break;
+                        case 3:
+                            mPagamento = MetodoPagamento.CREDITO;
+                            break;
+                        case 4:
+                            mPagamento = MetodoPagamento.VALE_ALIMENTACAO;
+                            break;
+                        case 5:
+                            mPagamento = MetodoPagamento.VALE_REFEICAO;
+                            break;
+                        default:
+                            break;
+                    }
+                    listaPedidos.add(new Pedido(descricao, preco, mPagamento));
                     break;
                 case 2:
                     System.out.println("verificar pedidos");
