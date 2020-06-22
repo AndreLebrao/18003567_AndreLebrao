0d30539e2936bab269f005b6875b304fe0612246
André Lebrão <61150819+andledrao@users.noreply.github.com>
2020-06-22 10:42:52 -0300

    javadocs enum EstadoPedido

--- Linguagens1_Projetos/ProjetoP1/src/br/maua/enums/EstadoPedido.java
+++ Linguagens1_Projetos/ProjetoP1/src/br/maua/enums/EstadoPedido.java
@@ -1,5 +1,8 @@
 package br.maua.enums;
-
+/**
+ * Enumeracao de todos os possíveis estados de um objeto Pedido
+ * @see Pedido
+ */
 public enum EstadoPedido {
     REALIZADO, PREPARACAO,SAIU_PARA_ENTREGA, ENTREGUE, DEVOLVIDO;
 }
