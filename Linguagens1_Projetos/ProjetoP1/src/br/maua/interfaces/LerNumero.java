b24f75da9104cc58a5c3d9f013c02a3e6d5112a0
André Lebrão <61150819+andledrao@users.noreply.github.com>
2020-06-22 10:42:25 -0300

    javadocs interface LerNumero

--- Linguagens1_Projetos/ProjetoP1/src/br/maua/interfaces/LerNumero.java
+++ Linguagens1_Projetos/ProjetoP1/src/br/maua/interfaces/LerNumero.java
@@ -1,6 +1,11 @@
 package br.maua.interfaces;
 
 public interface LerNumero {
+    /**
+     * 
+     * @param numString String que se deseja converter para um numero inteiro
+     * @return o numero inteiro parseado para int, em caso de erro retorna 9999
+     */
     public static int lerInt(String numString) {
         try {
             return Integer.parseInt(numString);
@@ -8,7 +13,11 @@ public interface LerNumero {
             return 9999;
         }
     }
-
+    /**
+     * 
+     * @param numString String que se deseja converter para um double
+     * @return um numero parseado para double, em caso de erro retorna 0
+     */
     public static double lerDouble(String numString) {
         try {
             return Integer.parseInt(numString);
