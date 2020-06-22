fb3edb65699fc75a268c34ecd5abd5d58deb3ad4
André Lebrão <61150819+andledrao@users.noreply.github.com>
2020-06-22 10:42:42 -0300

    javadocs enum MetodoPagamento

--- Linguagens1_Projetos/ProjetoP1/src/br/maua/enums/MetodoPagamento.java
+++ Linguagens1_Projetos/ProjetoP1/src/br/maua/enums/MetodoPagamento.java
@@ -1,5 +1,9 @@
 package br.maua.enums;
-
+/**
+ * Enumeracao de todos os tipos de pagamento válidos para um objeto Pedido
+ * @see Pedido
+ * 
+ */
 public enum MetodoPagamento {
     DINHEIRO, DEBITO, CREDITO, VALE_ALIMENTACAO, VALE_REFEICAO;
 }
