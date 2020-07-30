3e4772ee113d42a80729b298c48ba23ca9c5d476
André Lebrão <61150819+andledrao@users.noreply.github.com>
2020-07-30 10:04:32 -0300

    data no constructor

--- EstruturaDados/AtividadesArvores/src/models/BinaryNode.java
+++ EstruturaDados/AtividadesArvores/src/models/BinaryNode.java
@@ -4,14 +4,16 @@ public class BinaryNode extends Node{
     BinaryNode left;
     BinaryNode right;
     
-    public BinaryNode(){
+    public BinaryNode(int data){
         this.left = null;
         this.right = null;
+        this.data = data;
     }
     
-    public BinaryNode(BinaryNode left, BinaryNode right){
+    public BinaryNode(int data,BinaryNode left, BinaryNode right){
         this.left = left;
         this.right = right;
+        this.data = data;
     }
 }
 
