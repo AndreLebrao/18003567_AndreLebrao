d49c51b77753badfa49e48035c304f13f9ce6e36
André Lebrão <61150819+andledrao@users.noreply.github.com>
2020-08-10 12:11:40 -0300

    produtoparser

---
+++ Linguagens1_Projetos/ProjetoArquivo/src/parsers/ProdutoParser.java
@@ -0,0 +1,20 @@
+package parsers;
+
+import org.json.JSONObject;
+
+import models.Produto;
+
+public class ProdutoParser {
+    public static JSONObject toJson(Produto produto){
+        JSONObject json = new JSONObject();
+        json.put("valor", produto.getValor());
+        json.put("descricao", produto.getDescricao());
+        json.put("quantidade", produto.getQuantidade());
+        return json;
+    }
+
+    public static Produto fromJson(JSONObject json){
+        Produto produto = new Produto(json.getDouble("valor"),json.getString("descricao"),json.getInt("quantidade"));
+        return produto;
+    }
+}
