5f46fc16e09827043d85041d753fa97d9766718d
André Lebrão <61150819+andledrao@users.noreply.github.com>
2020-08-10 12:16:11 -0300

    Arquivo

--- Linguagens1_Projetos/ProjetoArquivo/src/parsers/ProdutoParser.java
+++ Linguagens1_Projetos/ProjetoArquivo/src/parsers/ProdutoParser.java
@@ -18,3 +18,4 @@ public class ProdutoParser {
         return produto;
     }
 }
+
---
+++ Linguagens1_Projetos/ProjetoArquivo/src/serializacao/Arquivo.java
@@ -0,0 +1,57 @@
+package serializacao;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+import org.json.JSONArray;
+
+public class Arquivo {
+    
+    private static void escreverArquivo(String nomeArquivo, JSONArray json) {
+        //Cria um arquivo
+        File file = new File(nomeArquivo);
+        try{
+
+            //Cria uma conexão apra escrever no arquivo
+            FileWriter fileWriter = new FileWriter(file);
+            //Cria um buffer de writting
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            //Escrever algo no arquivo
+            bufferedWriter.write("" + json);
+
+            //Fecha o buffer e escreve no arquivo
+            bufferedWriter.close();
+            //fecha os demais fluxos
+            fileWriter.close();
+
+        }catch(Exception exception){
+            System.out.println("Algo deu errado!");
+            exception.printStackTrace();
+        }
+    }
+    private static JSONArray lerArquivo(String nomeArquivo) {
+        JSONArray json = null;
+        File file = new File(nomeArquivo);
+        try{
+            //Cria o fluxo de leitura
+            FileReader fileReader = new FileReader(file);
+            //Buffer de leitura
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            json = new JSONArray (bufferedReader.readLine());
+
+            //Feche tudo ao terminar
+            bufferedReader.close();
+            fileReader.close();
+
+        }catch (Exception exception){
+            System.out.println("Algo deu errado!");
+            exception.printStackTrace();
+        }
+        return json;
+    }
+}
