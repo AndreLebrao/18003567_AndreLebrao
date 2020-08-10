19bba98f3d5fcb2b875ffd773eeedca2bae9866f
André Lebrão <61150819+andledrao@users.noreply.github.com>
2020-08-10 12:37:49 -0300

    editarquantidade

--- Linguagens1_Projetos/ProjetoArquivo/src/controller/EstoqueController.java
+++ Linguagens1_Projetos/ProjetoArquivo/src/controller/EstoqueController.java
@@ -2,11 +2,13 @@ package controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import models.Produto;
 
 public class EstoqueController {
     private List<Produto> estoque = new ArrayList<>();
+    final static Scanner sc = new Scanner(System.in);
 
     public EstoqueController(){}
 
@@ -15,12 +17,24 @@ public class EstoqueController {
     }
     public void deleteProduto(){
         this.getListEstoque();
-
+        int resposta = sc.nextInt();
+        sc.nextLine();
+        this.estoque.remove(resposta-1);
     }
 
     public void getListEstoque(){
+        System.out.println("Lista do estoque:");
         for (Produto produto : this.estoque){
             System.out.println(produto);
         }
+    }
+    public void editarQuantidade(){
+        this.getListEstoque();
+        int resposta = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Nova quantidade: ");
+        int quantidade = sc.nextInt();
+        sc.nextLine();
+        this.estoque.get(resposta-1).setQuantidade(quantidade);
     }
 }
