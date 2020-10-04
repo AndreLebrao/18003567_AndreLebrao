3ebdf673029ad7cb2922292a96ca7abc10113938
André Lebrão <61150819+andledrao@users.noreply.github.com>
2020-10-04 12:01:53 -0300

    adicionando TODO

--- Linguagens1_Projetos/T3/src/br/maua/models/CLI.java
+++ Linguagens1_Projetos/T3/src/br/maua/models/CLI.java
@@ -14,10 +14,12 @@ public class CLI {
         int opcao;
         String nome;
         while(flag){
-            System.out.printf("1 - Anime\n2 - Manga\n0 - sair\nEscolha a opção: ");
+            System.out.printf("1 - Anime\n2 - Manga\n3 - Mostrar banco de dados\n0 - sair\nEscolha a opção: ");
             opcao = scanner.nextInt();
             scanner.nextLine();
             switch (opcao){
+                //TODO: antes de pesquisar na API tem que pesquisar no banco de dados, se ja tiver ja fala que tem no
+                // banco
                 case 1:
                     System.out.println("Nome do anime: ");
                     nome = scanner.nextLine();
@@ -29,6 +31,21 @@ public class CLI {
                     nome = scanner.nextLine();
                     Manga manga = MangaParser.fromJSON(JikanAPI.getFirstSearchResult(Tipo.MANGA,nome));
                     System.out.println(manga);
+                    break;
+                case 3:
+                    System.out.println("Qual banco de dados?\n1 - Anime\n2 - Manga\nEscolha a opção: ");
+                    int opcaoBanco = scanner.nextInt();
+                    scanner.nextLine();
+                    switch (opcaoBanco){
+                        case 1:
+                            System.out.println("banco anime");
+                            break;
+                        case 2:
+                            System.out.println("banco manga");
+                            break;
+                        default:
+                            break;
+                    }
                     break;
                 case 0:
                     flag=false;
