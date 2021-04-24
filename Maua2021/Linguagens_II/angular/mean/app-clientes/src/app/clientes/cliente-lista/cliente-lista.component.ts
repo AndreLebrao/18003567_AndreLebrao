63373988ca87d2c8e24e905aa8beae660f8e85cf
André Lebrão <61150819+andledrao@users.noreply.github.com>
2021-04-24 12:04:55 -0300

    fim apostila 1

--- Maua2021/Linguagens_II/angular/mean/app-clientes/src/app/clientes/cliente-lista/cliente-lista.component.ts
+++ Maua2021/Linguagens_II/angular/mean/app-clientes/src/app/clientes/cliente-lista/cliente-lista.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cliente-lista',
@@ -6,6 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cliente-lista.component.css']
 })
 export class ClienteListaComponent implements OnInit {
+
+  // clientes=[
+  //   {
+  //     nome:'jose',
+  //     email:'jose@gmail.com',
+  //     fone:'12315135'
+
+  //   },
+  //   {
+  //     nome:'maria',
+  //     email:'maria@gmail.com',
+  //     fone:'3847617356'
+
+  //   },
+  // ]
+
+  @Input() clientes=[]
 
   constructor() { }
 
